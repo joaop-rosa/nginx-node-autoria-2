a1cb1a338e6a7041ac34e2f8d8652d5350f2031a
Xonho <jp.ragdool@hotmail.com>
2023-08-24 23:49:01 -0300

    adicionado readme e ajustado configs nginx

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ const app = express();
 const path = require("path");
 const SERVER_PORT = 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  const paginaPath = path.join(__dirname, "index.html");
-  res.sendFile(paginaPath);
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/curriculo", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(SERVER_PORT, () => {
