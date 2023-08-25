029f3e6845bf71b847e0d3db0f79a0515c6e1000
Xonho <jp.ragdool@hotmail.com>
2023-08-25 00:47:06 -0300

    ajustes api e nginx

--- api/index.js
+++ api/index.js
@@ -5,10 +5,6 @@ const SERVER_PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 app.get("/curriculo", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
